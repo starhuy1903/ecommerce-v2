768d2b6254ccabac499ee95e5c8751634f7e7f61
Huy Nguyen <huykst137@gmail.com>
2022-09-22 09:31:38 +0700

    create pure React slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from "../components/Navbar";
 import Announcement from "../components/Anouncement";
+import Slider from "../components/Slider";
 
 const Home = () => {
     return (
         <div>
             <Announcement />
             <Navbar />
+            <Slider />
         </div>
     );
 };
